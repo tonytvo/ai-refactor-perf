b87d896430a28cc939b41a6c4af0340fa0b1c594
Tony Vo <ttrung.vo@gmail.com>
2025-06-27 14:58:16 -0600

    add sample test

---
+++ src/test/java/org/sample/PrimeFinderTest.java
@@ -0,0 +1,12 @@
+package org.sample;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimeFinderTest {
+    @Test
+    void alwaysFails() {
+        fail("This test always fails");
+    }
+}
